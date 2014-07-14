2819b3cb741aaf25d216be5028c26552907f5096
Jeff Kehler <jeffrey.kehler@gmail.com>
2014-07-14 11:48:59 +0700

    Cleanup

--- app/src/main/java/com/appinforium/newthinktankcodingtutorials/adapter/PlaylistCursorAdapter.java
+++ app/src/main/java/com/appinforium/newthinktankcodingtutorials/adapter/PlaylistCursorAdapter.java
@@ -1,16 +1,12 @@
 package com.appinforium.newthinktankcodingtutorials.adapter;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,12 +18,8 @@ import com.appinforium.newthinktankcodingtutorials.R;
 import com.appinforium.newthinktankcodingtutorials.data.YoutubeDatabase;
 import com.squareup.picasso.Picasso;
 
-import java.io.ByteArrayInputStream;
 import java.util.Random;
 
-/**
- * Created by jeff on 7/11/14.
- */
 public class PlaylistCursorAdapter extends CursorAdapter {
 
     LayoutInflater inflater;
@@ -68,19 +60,6 @@ public class PlaylistCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-//        TextView titleTextView = (TextView) view.findViewById(R.id.videoTitleTextView);
-//        titleTextView.setText(cursor.getString(cursor.getColumnIndex(YoutubeDatabase.COL_TITLE)));
-
-//        ImageView thumbnailImageView = (ImageView) view.findViewById(R.id.videoThumbnailImageView);
-//        byte[] thumbnailBytes = cursor.getBlob(cursor.getColumnIndex(YoutubeDatabase.COL_THUMBNAIL_BITMAP));
-//        if (thumbnailBytes == null) {
-//            thumbnailImageView.setImageDrawable(context.getResources().getDrawable(R.drawable.no_thumbnail));
-//        } else {
-//            ByteArrayInputStream imageStream = new ByteArrayInputStream(thumbnailBytes);
-//            Bitmap thumbnailBitmap = BitmapFactory.decodeStream(imageStream);
-//            thumbnailImageView.setImageBitmap(thumbnailBitmap);
-//
-//        }
 
         ViewHolder holder = (ViewHolder) view.getTag();
 
--- app/src/main/java/com/appinforium/newthinktankcodingtutorials/adapter/PlaylistsCursorAdapter.java
+++ app/src/main/java/com/appinforium/newthinktankcodingtutorials/adapter/PlaylistsCursorAdapter.java
@@ -3,31 +3,22 @@ package com.appinforium.newthinktankcodingtutorials.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.appinforium.newthinktankcodingtutorials.R;
 import com.appinforium.newthinktankcodingtutorials.data.YoutubeDatabase;
-import com.appinforium.newthinktankcodingtutorials.data.YoutubeProvider;
 import com.squareup.picasso.Picasso;
-
-import java.io.ByteArrayInputStream;
 import java.util.Random;
 
-/**
- * Created by jeff on 7/11/14.
- */
+
 public class PlaylistsCursorAdapter extends CursorAdapter {
 
     LayoutInflater inflater;
@@ -66,22 +57,8 @@ public class PlaylistsCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-//        TextView titleTextView = (TextView) view.findViewById(R.id.playlistTitleTextView);
-//        titleTextView.setText(cursor.getString(cursor.getColumnIndex(YoutubeDatabase.COL_TITLE)));
 
-//        ImageView thumbnailImageView = (ImageView) view.findViewById(R.id.playlistThumbnailImageView);
         ViewHolder holder = (ViewHolder) view.getTag();
-
-
-//        byte[] thumbnailBytes = cursor.getBlob(cursor.getColumnIndex(YoutubeDatabase.COL_THUMBNAIL_BITMAP));
-//        if (thumbnailBytes == null) {
-//            holder.thumbnail.setImageDrawable(context.getResources().getDrawable(R.drawable.no_thumbnail));
-//        } else {
-//            ByteArrayInputStream imageStream = new ByteArrayInputStream(thumbnailBytes);
-//            Bitmap thumbnailBitmap = BitmapFactory.decodeStream(imageStream);
-//            holder.thumbnail.setImageBitmap(thumbnailBitmap);
-//
-//        }
 
         Picasso.with(context)
                 .load(cursor.getString(cursor.getColumnIndex(YoutubeDatabase.COL_THUMBNAIL_URL)))
